3d7330d1d684f6180ffa63f973fcdfea7f290787
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-10-22 18:11:13 -0500

    Adds TypeOrmModule.forRoot (...) that contains database connection information. To see the error, go to nearest parent in master. Nestjs couldn't resolve dependencies because it was missing a Connection object. Showed up from tryiny to resolve CompanyRepository dependencies.

--- developer.md
+++ developer.md
@@ -1,3 +1,5 @@
 nest new crud
 yarn add @nestjsx/crud class-transformer class-validator
 nest g module companies
+
+5790bd1: Nestjs cannot resolve dependencies
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [
+    CompaniesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'db_user',
+      password: 'db_password',
+      database: 'db_app',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
