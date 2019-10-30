d905f0bbe0e0affeaaf527769cbb7575c39d4e0e
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-10-29 20:00:09 -0500

    Build commands now started from docker-file

--- run-using-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Look in the current directory '.' for 'Dockerfile' for instructions to build an image and tag (i.e. name via '-t') it 'quaffpotion/nest-crud':
-docker build -t quaffpotion/nest-crud .
-
-#Have docker-compose look at 'docker-compose.yml' for instructions to create our images on the 'nest-net' docker network:
-docker-compose up
